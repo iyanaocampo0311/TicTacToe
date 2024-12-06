9e1dbcf695419048bd879792655c37b83fc9b19c
ramesh <rameshhalder75@gmail.com>
2024-12-05 21:40:46 -0800

    Testing a pull request by removing an unused import

--- src/test/java/co/ppg2/controllers/PlayerDataControllerTest.java
+++ src/test/java/co/ppg2/controllers/PlayerDataControllerTest.java
@@ -1,37 +1,58 @@
 package co.ppg2.controllers;
 
-
 import co.ppg2.model.Player;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 
 import java.util.ArrayList;
-
+import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 class PlayerDataControllerTest {
 
+    private ArrayList<Player> players;
+    private static final String TEST_FILE_PATH = "test_players.dat";
 
-    @Test
-    void testSaveAndLoadPlayers() {
-        ArrayList<Player> players = new ArrayList<>();
+    @BeforeEach
+    void setUp() {
+        players = new ArrayList<>();
         players.add(new Player("Player1"));
         players.add(new Player("Player2"));
 
+        // Set test file path for PlayerDataController
+        PlayerDataController.setFilePath(TEST_FILE_PATH);
+    }
 
+    @AfterEach
+    void tearDown() {
+        // Clean up test file after each test
+        File file = new File(TEST_FILE_PATH);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
+    @Test
+    void testSaveAndLoadPlayers() {
         PlayerDataController.savePlayers(players);
-
 
         ArrayList<Player> loadedPlayers = PlayerDataController.loadPlayers();
         assertEquals(2, loadedPlayers.size());
         assertEquals("Player1", loadedPlayers.get(0).getUsername());
         assertEquals("Player2", loadedPlayers.get(1).getUsername());
     }
+
+    // TODO: Add test for saving and loading an empty list of players
+
+    // TODO: Add test for saving and loading a large number of players
+
+    // TODO: Add test for saving and loading players with special characters in their names
+
+    // TODO: Add test for handling file I/O exceptions during save and load operations
+
+    // TODO: Add test for loading players when the file doesn't exist
+
+    // TODO: Add test for updating existing players' data
 }
-
-
-
-
-
--- src/test/java/co/ppg2/model/PlayerTest.java
+++ src/test/java/co/ppg2/model/PlayerTest.java
@@ -54,6 +54,19 @@ class PlayerTest {
     void testToString() {
         assertEquals("PlayerX - Wins: 0, Losses: 0", player.toString());
     }
+    // TODO: Add test for creating a player with an empty username
+
+    // TODO: Add test for creating a player with a very long username
+
+    // TODO: Add test for multiple increments of wins and losses
+
+    // TODO: Add test for resetting player statistics
+
+    // TODO: Add test for comparing two Player objects
+
+    // TODO: Add test for player with maximum possible wins and losses
+
+    // TODO: Add test for player name with special characters
 }
 
 

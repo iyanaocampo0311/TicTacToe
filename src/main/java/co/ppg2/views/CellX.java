00e1db4ec1900514821811ddbd73994a66a29062
Rabit3 <kevin.vinod07@gmail.com>
2024-12-07 17:18:59 -0800

    Update CellX.java

--- src/main/java/co/ppg2/views/CellX.java
+++ src/main/java/co/ppg2/views/CellX.java
@@ -1,21 +1,18 @@
 package co.ppg2.views;
 
-
 import javafx.scene.shape.Line;
-
 
 public class CellX extends CellBase {
     public CellX() {
+        // TODO: Add comments explaining that we create two lines for 'X'
         Line line1 = new Line(10, 10, 90, 90);
         Line line2 = new Line(90, 10, 10, 90);
         this.getChildren().addAll(line1, line2);
     }
 
-
-
-
     @Override
     public void setToken(char token) {
+        // TODO: Make sure this method is correctly setting the token for 'X'
         this.token = 'X';
     }
 }

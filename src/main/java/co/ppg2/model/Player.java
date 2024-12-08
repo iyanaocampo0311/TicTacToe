4899e99d76517e6236a48f28d103618bc1276219
dnzlv <denzelalt@gmail.com>
2024-12-07 16:00:51 -0800

    I found your group's project to be pretty solid, covering most of the course modules and being a great simple game. However there could possibly be some changes to improve the style of life changes. For example, I made the window dimensions larger and made it so that both players must enter a valid username to play or else an error button pops up.
     And I proposed ideas such as making a pause timer button as well as displaying the turn of the players in a more visible place. I also believe that getting rid of blank lines would make the readability better.

--- src/main/java/co/ppg2/model/Player.java
+++ src/main/java/co/ppg2/model/Player.java
@@ -3,11 +3,12 @@ package co.ppg2.model;
 import java.io.Serializable;
 
 public class Player implements Serializable {
-    private final String username;
+    private String username;
     private int wins;
     private int losses;
 
     public Player(String username) {
+        setUsername(username);
         this.username = username;
         this.wins = 0;
         this.losses = 0;
@@ -15,6 +16,14 @@ public class Player implements Serializable {
 
     public String getUsername() {
         return username;
+    }
+
+    public void setUsername(String username) {
+        //added to make sure username is not empty
+        if (username == null || username.trim().isEmpty()) {
+            throw new IllegalArgumentException("Username can't be empty. Please enter a valid username.");
+        }
+        this.username = username;
     }
 
     public int getWins() {

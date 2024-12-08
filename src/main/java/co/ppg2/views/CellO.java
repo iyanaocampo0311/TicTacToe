ad05c0fd86e500f36b93704a0b88c9dace5b8588
Rabit3 <kevin.vinod07@gmail.com>
2024-12-07 17:18:19 -0800

    Update CellO.java

--- src/main/java/co/ppg2/views/CellO.java
+++ src/main/java/co/ppg2/views/CellO.java
@@ -1,26 +1,22 @@
 package co.ppg2.views;
 
-
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Ellipse;
 
-
 public class CellO extends CellBase {
     public CellO() {
-        Ellipse ellipse = new Ellipse(50, 50, 40, 40);
-        ellipse.setStroke(Color.BLACK);
-        ellipse.setFill(Color.WHITE);
-        this.getChildren().add(ellipse);
+        // TODO: Maybe allow color change for "O" in the future
+        Ellipse ellipse = new Ellipse(50, 50, 40, 40); // Create shape for "O"
+        ellipse.setStroke(Color.BLACK); // Set outline color
+        ellipse.setFill(Color.WHITE); // Set fill color
+        this.getChildren().add(ellipse); // Add the shape to the cell
     }
-
 
     @Override
     public void setToken(char token) {
-        this.token = 'O';
+        // TODO: Check if token is valid ('O')
+        this.token = 'O'; // Set token to 'O'
     }
+
+    // TODO: Add a reset method to clear the cell
 }
-
-
-
-
-
